5effa5a0b54b972782c25715875e66ea18deb25c
jprogramcourses <jprogramcourses@gmail.com>
2023-03-14 20:20:50 +0100

    First commit

---
+++ pr_tec_san/src/main/java/com/prueba/servsanitcalc/PrTecSanApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.servsanitcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrTecSanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrTecSanApplication.class, args);
+	}
+
+}
---
+++ pr_tec_san/src/test/java/com/prueba/servsanitcalc/PrTecSanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.servsanitcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrTecSanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
